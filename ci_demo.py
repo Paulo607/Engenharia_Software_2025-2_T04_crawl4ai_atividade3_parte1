c0641b0119d72ea9ddb8f0ec3b6aed3adc3b627b
Paulo607 <paulohenriquecarvalho607@gmail.com>
2026-02-05 19:57:06 -0300

    Create ci_demo.py

---
+++ ci_demo.py
@@ -0,0 +1,8 @@
+def test_simples():
+    # Uma lógica simples que sempre passará
+    resultado = 1 + 1
+    assert resultado == 2
+    print("Sucesso: A lógica básica de integração está funcionando!")
+
+if __name__ == "__main__":
+    test_simples()
